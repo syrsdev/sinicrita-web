5402379587deaa8dbe032625465bdb7439dac789
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-04-14 11:28:24 +0700

    feat: handle

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,10 +4,23 @@ import Input from "../components/input/Index";
 import Button from "../components/Button";
 import Label from "../components/input/Label";
 import RadioButton from "../components/input/Radio";
+import { useState } from "react";
+import { Register } from "../services/auth.service";
 
 const RegisterPage = () => {
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+    const [Role, setRole] = useState("pendengar");
+
+    const data = { username: Username, password: Password, role: Role };
+    console.log(data);
+
+    const handleRegister = (e: any) => {
+        e.preventDefault();
+        Register(data);
+    };
     return (
-        <AuthLayout>
+        <AuthLayout onsubmit={handleRegister}>
             <h1 className="font-bold text-[23px] md:text-[28px] xl:text-[40px] text-center">
                 Register
             </h1>
@@ -15,18 +28,29 @@ const RegisterPage = () => {
                 label="Username"
                 autoFocus={true}
                 placeholder="Masukan username"
+                onchange={(e: any) => setUsername(e.target.value)}
             ></Input>
             <Input
                 label="Password"
                 type="password"
                 placeholder="Masukan password"
+                onchange={(e: any) => setPassword(e.target.value)}
             ></Input>
 
             <div className="flex-col">
                 <Label label="Role" />
                 <div className="flex gap-6">
-                    <RadioButton label="Pendengar" name="role" checked={true} />
-                    <RadioButton label="Pencerita" name="role" />
+                    <RadioButton
+                        label="Pendengar"
+                        name="role"
+                        checked={true}
+                        onchange={() => setRole("pendengar")}
+                    />
+                    <RadioButton
+                        label="Pencerita"
+                        name="role"
+                        onchange={() => setRole("pencerita")}
+                    />
                 </div>
             </div>
 
