36b9f3d28e88d14f86ad730a13bd41c9edc1965b
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-04-14 11:28:16 +0700

    feat: handle login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,10 +2,22 @@ import { NavLink } from "react-router-dom";
 import AuthLayout from "../layout/AuthLayout";
 import Input from "../components/input/Index";
 import Button from "../components/Button";
+import { useState } from "react";
+import { Login } from "../services/auth.service";
+import { Alert } from "../hooks/useAlert";
 
 const LoginPage = () => {
+    const [Username, setUsername] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const data = { username: Username, password: Password };
+
+    const handleLogin = (e: any) => {
+        e.preventDefault();
+        Login(data);
+    };
     return (
-        <AuthLayout>
+        <AuthLayout onsubmit={handleLogin}>
             <h1 className="font-bold text-[23px] md:text-[28px] xl:text-[40px] text-center">
                 Login
             </h1>
@@ -13,11 +25,13 @@ const LoginPage = () => {
                 label="Username"
                 autoFocus={true}
                 placeholder="Masukan username"
+                onchange={(e: any) => setUsername(e.target.value)}
             ></Input>
             <Input
                 label="Password"
                 type="password"
                 placeholder="Masukan password"
+                onchange={(e: any) => setPassword(e.target.value)}
             ></Input>
 
             <Button type="submit" classname="mt-4">
