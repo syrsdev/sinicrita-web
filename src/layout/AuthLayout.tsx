50208368c9a06fc44749ee5c23d2d8d4679e9efe
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-04-14 11:28:03 +0700

    fix: bg

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,7 +1,10 @@
-const AuthLayout = ({ children }: any) => {
+const AuthLayout = ({ children, onsubmit }: any) => {
     return (
-        <div className="h-screen w-f flex justify-center items-center text-white bg-gradient-to-bl from-primary via-40% via-tertiary to-white">
-            <form className="flex bg-primary p-7 md:p-10 rounded-2xl gap-7 w-10/12 md:w-1/2 lg:w-1/3 flex-col">
+        <div className="min-h-screen w-f flex justify-center items-center text-white bg-gradient-to-bl from-primary via-40% via-tertiary to-white">
+            <form
+                onSubmit={onsubmit}
+                className="flex bg-primary p-7 md:p-10 rounded-2xl gap-7 w-10/12 md:w-1/2 lg:w-1/3 flex-col"
+            >
                 {children}
             </form>
         </div>
