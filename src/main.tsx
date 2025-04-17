c2309cd79edd466e3fc39f894dd749ba0b64e353
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-04-17 15:51:29 +0700

    chore: change app.tsx to home.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
-import App from "./App.tsx";
+import App from "./pages/Home.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/Login.tsx";
 import RegisterPage from "./pages/Register.tsx";
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { useAlert } from "../hooks/useAlert";
+
+function App() {
+    useAlert();
+
+    return (
+        <>
+            <div>tes</div>
+        </>
+    );
+}
+
+export default App;
