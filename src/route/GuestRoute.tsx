c06208f317908021182c7ebd8489baaf4b32020d
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-05-05 02:21:32 +0700

    feat: loading middleware check

--- src/route/GuestRoute.tsx
+++ src/route/GuestRoute.tsx
@@ -3,8 +3,9 @@ import useAuth from "../hooks/useAuth";
 import { JSX } from "react";
 
 const GuestRoute = ({ children }: { children: JSX.Element }) => {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
 
+    if (loading) return <p>Loading...</p>
     if (user) return <Navigate to="/post" replace />;
 
     return children;
--- src/route/ProtectedRoute.tsx
+++ src/route/ProtectedRoute.tsx
@@ -3,8 +3,9 @@ import useAuth from "../hooks/useAuth";
 import { JSX } from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
 
+    if (loading) return <p>Loading...</p>
     if (!user) return <Navigate to="/login" replace />;
 
     return children;
