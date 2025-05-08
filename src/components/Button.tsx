6b50a1149cc5c60167bca4f2372798a2c33f6f7a
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-05-08 21:15:09 +0700

    style: add bg props in button comp

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,25 @@
 import Loading from "./Loading";
 
 const Button = ({
-    children,
-    type = "button",
-    classname,
-    disable,
-    onclick,
+  children,
+  type = "button",
+  bg = "bg-secondary",
+  classname,
+  disable,
+  onclick,
 }: any) => {
-    return (
-        <button
-            type={type}
-            disabled={disable}
-            onClick={onclick}
-            className={`w-full gap-2 flex justify-center items-center bg-secondary py-2 px-3 rounded-lg ${
-                disable ? "cursor-not-allowed" : "cursor-pointer"
-            } ${classname}`}
-        >
-            {disable ? <Loading /> : children}
-        </button>
-    );
+  return (
+    <button
+      type={type}
+      disabled={disable}
+      onClick={onclick}
+      className={`w-full gap-2 flex justify-center items-center ${bg} py-2 px-3 rounded-lg ${
+        disable ? "cursor-not-allowed" : "cursor-pointer"
+      } ${classname}`}
+    >
+      {disable ? <Loading /> : children}
+    </button>
+  );
 };
 
 export default Button;
