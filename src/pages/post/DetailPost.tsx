df3e89b7a4bc0b4eeafd4ca253850ec41244a005
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-05-13 17:37:09 +0700

    style: condition edit button

--- src/pages/post/DetailPost.tsx
+++ src/pages/post/DetailPost.tsx
@@ -3,9 +3,12 @@ import useAuth from "../../hooks/useAuth";
 import MainLayout from "../../layout/MainLayout";
 import MainSidebar from "../../components/sidebar/MainSidebar";
 import { detailPost } from "../../services/post.service";
+import Button from "../../components/button/Button";
+import ButtonSecondary from "../../components/button/ButtonSecondary";
 
 type Post = {
   user: {
+    id: number | undefined;
     username: string;
   };
   content: string;
@@ -14,7 +17,10 @@ type Post = {
 
 const DetailPost = () => {
   const [post, setPost] = useState({} as Post);
+  const [IsEdit, setIsEdit] = useState(false);
+  const [content, setContent] = useState("");
   const { user } = useAuth();
+  const formatedDate = new Date(post?.created_at);
 
   useEffect(() => {
     detailPost(window.location.pathname, (res) =>
@@ -24,12 +30,55 @@ const DetailPost = () => {
 
   return (
     <MainLayout title="Cerita" sidebar={<MainSidebar />} userLogin={user}>
-      <div className="p-10 bg-white w-full m-10 rounded-xl flex flex-col gap-2">
-        <h1 className="text-[22px] font-bold">
-          Dibuat oleh{" "}
-          <span className="text-primary">@{post?.user?.username}</span>
-        </h1>
-        <p className="text-[16px]">{post?.content}</p>
+      <div className="p-10 bg-white w-full m-10 rounded-2xl flex flex-col gap-2">
+        <div className="flex flex-col gap-1 mb-4">
+          <h1 className="text-[22px] font-bold">
+            Dibuat oleh{" "}
+            <span className="text-primary">@{post?.user?.username}</span>
+          </h1>
+          <p>
+            Pada{" "}
+            {formatedDate.toLocaleDateString("id-ID", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </p>
+        </div>
+        {IsEdit == false && <p className="text-[16px]">{post?.content}</p>}
+
+        {user?.id == post?.user?.id && (
+          <form>
+            {IsEdit == true && (
+              <textarea
+                minLength={5}
+                autoFocus
+                name="content"
+                id="content"
+                className="bg-white w-full rounded-2xl focus:outline-primary p-4 min-h-80"
+                placeholder="Tulis cerita anda..."
+                onChange={(e) => setContent(e.target.value)}
+              >
+                {post?.content}
+              </textarea>
+            )}
+
+            <div className="flex gap-8 justify-between mt-10">
+              <Button
+                onclick={() => setIsEdit(!IsEdit)}
+                bg="bg-primary hover:bg-[#2DB7B4FF]"
+                classname="text-white"
+              >
+                {IsEdit == true ? "Batalkan" : "Edit"}
+              </Button>
+              {IsEdit == true ? (
+                <ButtonSecondary>Simpan</ButtonSecondary>
+              ) : (
+                <ButtonSecondary>Hapus</ButtonSecondary>
+              )}
+            </div>
+          </form>
+        )}
       </div>
     </MainLayout>
   );
