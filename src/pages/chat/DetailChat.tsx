1eff3e51290795afec3b5dba34e81dee0e4a8e81
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-13 00:56:08 +0700

    style: detail chat

--- src/pages/chat/DetailChat.tsx
+++ src/pages/chat/DetailChat.tsx
@@ -1,12 +1,41 @@
-import MainLayout from "../../layout/MainLayout";
 import useAuth from "../../hooks/useAuth";
-import SidebarChat from "../../components/sidebar/SidebarChat";
 import { Link } from "react-router-dom";
+import FieldChat from "./FieldChat";
+import { getDetailChat } from "../../services/chat.service";
+import { useEffect, useState } from "react";
 
-const DetailChat = () => {
+const DetailChat = ({ id }: { id: number }) => {
   const { user } = useAuth();
+  const [chat, setChat] = useState([]);
 
-  return <p>details</p>;
+  useEffect(() => {
+    if (!Number.isNaN(id)) {
+      getDetailChat(id, (res) => {
+        setChat(res.data.data);
+      });
+    }
+  }, [id]);
+  console.log(chat);
+
+  return (
+    <div className="flex flex-col w-full justify-between pb-[70px] h-screen">
+      <div className="w-full">
+        <div className="flex flex-col gap-2 ">
+          {chat.map((item: any) => (
+            <p
+              key={item.id}
+              className={`bg-white p-5 ${
+                item.sender_id == user?.id ? "ms-auto" : "me-auto"
+              }`}
+            >
+              {item.message}
+            </p>
+          ))}
+        </div>
+      </div>
+      <FieldChat></FieldChat>
+    </div>
+  );
 };
 
 export default DetailChat;
--- src/pages/chat/FieldChat.tsx
+++ src/pages/chat/FieldChat.tsx
@@ -1,15 +1,28 @@
-import Input from "../../components/input/Index";
 import Button from "../../components/button/Button";
 import { IoMdSend } from "react-icons/io";
+import { useEffect, useRef, useState } from "react";
 
 const FieldChat = () => {
+  const [value, setValue] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [value]);
   return (
-    <form className="flex justify-between items-center gap-3 bg-primary p-5 rounded-2xl">
-      <Input
-        autoFocus={true}
-        placeholder="Kirim Tanggapan..."
-        // onchange={(e: any) => setUsername(e.target.value)}
-      ></Input>
+    <form className="flex justify-between items-center gap-3 bg-primary p-3 w-full">
+      <textarea
+        ref={textareaRef}
+        rows={1}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Tulis sesuatu..."
+        autoFocus
+        className="w-full resize-none overflow-hidden rounded border bg-white border-gray-300 px-4 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+      />
       <div className="w-fit text-white">
         <Button>
           <IoMdSend />
