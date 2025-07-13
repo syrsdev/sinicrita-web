0a9d4ac7a9c938648281a94daab48063885d2110
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-13 18:51:41 +0700

    chore:fix unauthenticated user

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -2,65 +2,103 @@ import { AxiosResponse } from "axios";
 import api from "./axios.service";
 import { alert } from "../hooks/useAlert";
 
-export const getPost = (user: String | undefined ,callback: (res: AxiosResponse) => void) => {
-    api
-        .get(`/post/${user}`)
-        .then((res) => {
-            callback(res);
-        })
-        .catch((err) => {
-            return callback(err.response);
+export const getPost = (
+  user: String | undefined,
+  callback: (res: AxiosResponse) => void
+) => {
+  api
+    .get(`/post/${user}`)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      if (err.response.status != 401) {
+        alert.fire({
+          title: "Oops...",
+          text: err.response.data.message,
+          icon: "error",
         });
+      } else {
+        window.location.href = "/";
+      }
+    });
 };
-export const detailPost = (slug: string, callback: (res: AxiosResponse) => void) => {
-    api
-        .get(`${slug}`)
-        .then((res) => {
-            callback(res);
-        })
-        .catch((err) => {
-            return callback(err.response);
+
+export const detailPost = (
+  slug: string,
+  callback: (res: AxiosResponse) => void
+) => {
+  api
+    .get(`${slug}`)
+    .then((res) => {
+      callback(res);
+    })
+    .catch((err) => {
+      if (err.response.status != 401) {
+        alert.fire({
+          title: "Oops...",
+          text: err.response.data.message,
+          icon: "error",
         });
+      } else {
+        window.location.href = "/";
+      }
+    });
 };
 
-export const createPost = (data:object) => {
-    api.post("/post", data)
+export const createPost = (data: object) => {
+  api
+    .post("/post", data)
     .then(() => {
-            window.location.href = "/post";
-        })
+      window.location.href = "/post";
+    })
     .catch((err) => {
+      if (err.response.status != 401) {
         alert.fire({
-            title: "Oops...",
-            text: err.response.data.message,
-            icon: "error",
+          title: "Oops...",
+          text: err.response.data.message,
+          icon: "error",
         });
+      } else {
+        window.location.href = "/";
+      }
     });
-}
+};
 
-export const updatePost = (data:object, slug:String) => {
-    api.put(`/post/detail/${slug}`, data)
+export const updatePost = (data: object, slug: String) => {
+  api
+    .put(`/post/detail/${slug}`, data)
     .then(() => {
-            window.location.href = `/post`;
-        })
+      window.location.href = `/post`;
+    })
     .catch((err) => {
+      if (err.response.status != 401) {
         alert.fire({
-            title: "Oops...",
-            text: err.response.data.message,
-            icon: "error",
+          title: "Oops...",
+          text: err.response.data.message,
+          icon: "error",
         });
+      } else {
+        window.location.href = "/";
+      }
     });
-}
+};
 
-export const deletePost = (slug:String) => {
-    api.delete(`/post/detail/${slug}/delete`)
+export const deletePost = (slug: String) => {
+  api
+    .delete(`/post/detail/${slug}/delete`)
     .then(() => {
-            window.location.href = `/post`;
-        })
+      window.location.href = `/post`;
+    })
     .catch((err) => {
+      if (err.response.status != 401) {
         alert.fire({
-            title: "Oops...",
-            text: err.response.data.message,
-            icon: "error",
+          title: "Oops...",
+          text: err.response.data.message,
+          icon: "error",
         });
+      } else {
+        window.location.href = "/";
+      }
     });
-}
+};
