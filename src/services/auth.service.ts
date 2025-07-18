4027509d0b62ef3dd79e0dc03241aa621fe2fa3c
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-19 00:57:20 +0700

    chore: remove console.log

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,63 +3,64 @@ import api from "./axios.service.ts";
 import { alert } from "../hooks/useAlert.ts";
 
 export const getCsrfToken = async (): Promise<void> => {
-    try {
-        await axios.get(`${import.meta.env.VITE_API_URL}/sanctum/csrf-cookie`, {
-            withCredentials: true,
-        });
-        console.log("CSRF token set");
-    } catch (error) {
-        console.error("Failed to fetch CSRF token:", error);
-    }
+  try {
+    await axios.get(`${import.meta.env.VITE_API_URL}/sanctum/csrf-cookie`, {
+      withCredentials: true,
+    });
+  } catch (error) {
+    console.error("Failed to fetch CSRF token:", error);
+  }
 };
 export const getUser = async () => {
-    try {
-        const data = await axios.get(
-            `${import.meta.env.VITE_API_URL}/api/v1/user`,
-            {
-                withCredentials: true,
-            }
-        );
-        return data.data;
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const data = await axios.get(
+      `${import.meta.env.VITE_API_URL}/api/v1/user`,
+      {
+        withCredentials: true,
+      }
+    );
+    return data.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const Login = (data: { username: string; password: string }) => {
-    getCsrfToken();
-    api.post("/login", data, {
-        withCredentials: true,
+  getCsrfToken();
+  api
+    .post("/login", data, {
+      withCredentials: true,
     })
-        .then(() => {
-            window.location.href = "/post";
-        })
-        .catch((err) => {
-            alert.fire({
-                title: "Oops...",
-                text: err.response.data.message,
-                icon: "error",
-            });
-        });
+    .then(() => {
+      window.location.href = "/post";
+    })
+    .catch((err) => {
+      alert.fire({
+        title: "Oops...",
+        text: err.response.data.message,
+        icon: "error",
+      });
+    });
 };
 export const Register = (data: {
-    username: string;
-    password: string;
-    role: string;
-    password_confirmation: string;
+  username: string;
+  password: string;
+  role: string;
+  password_confirmation: string;
 }) => {
-    getCsrfToken();
-    api.post("/register", data, {
-        withCredentials: true,
+  getCsrfToken();
+  api
+    .post("/register", data, {
+      withCredentials: true,
     })
-        .then(() => {
-            window.location.href = "/";
-        })
-        .catch((err) => {
-            alert.fire({
-                title: "Oops...",
-                text: err.response.data.message,
-                icon: "error",
-            });
-        });
+    .then(() => {
+      window.location.href = "/";
+    })
+    .catch((err) => {
+      alert.fire({
+        title: "Oops...",
+        text: err.response.data.message,
+        icon: "error",
+      });
+    });
 };
