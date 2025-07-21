547d74af09ccdc1c7085b3176387f410aca5d180
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-21 18:06:05 +0700

    fix: responsive add post

--- src/pages/post/AddPost.tsx
+++ src/pages/post/AddPost.tsx
@@ -31,26 +31,30 @@ const AddPost = () => {
       hiddenAddButton={true}
       userLogin={user}
     >
-      <form onSubmit={handleSubmit} className="w-full flex flex-col p-8 gap-10">
-        <textarea
-          // minLength={5}
-          autoFocus
-          name="content"
-          id="content"
-          className="bg-white w-full rounded-2xl focus:outline-primary p-4 min-h-60"
-          placeholder="Tulis cerita anda..."
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-
-        <Button
-          disable={buttonDisable}
-          type="submit"
-          classname="text-white"
-          bg="bg-primary"
+      <div className="w-full overflow-y-auto h-screen pb-[70px]">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col p-8 gap-10"
         >
-          Kirim
-        </Button>
-      </form>
+          <textarea
+            autoFocus
+            name="content"
+            id="content"
+            className="bg-white w-full rounded-2xl focus:outline-primary p-4 min-h-60"
+            placeholder="Tulis cerita anda..."
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+
+          <Button
+            disable={buttonDisable}
+            type="submit"
+            classname="text-white"
+            bg="bg-primary"
+          >
+            Kirim
+          </Button>
+        </form>
+      </div>
     </MainLayout>
   );
 };
