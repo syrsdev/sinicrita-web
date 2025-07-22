940e443d80ade0bd3859e0f43fe010636aa898d5
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-22 23:56:11 +0700

    chore: prettier

--- src/pages/chat/DetailChat.tsx
+++ src/pages/chat/DetailChat.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 import useAuth from "../../hooks/useAuth";
 import FieldChat from "./FieldChat";
 import dayjs from "dayjs";
