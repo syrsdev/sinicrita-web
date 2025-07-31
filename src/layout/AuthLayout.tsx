b13de8b1a72d8956335d38b804a90b0176a30a11
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-31 15:23:49 +0700

    style: add sinicrita logo

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
+
 const AuthLayout = ({ children, onsubmit }: any) => {
   return (
-    <div className="min-h-screen w-f flex justify-center items-center text-white bg-gradient-to-bl from-primary via-40% via-tertiary to-white">
-      <form
-        onSubmit={onsubmit}
-        className="flex bg-primary p-7 md:p-10 rounded-2xl gap-7 w-10/12 md:w-1/2 lg:w-1/3 flex-col"
-        method="post"
-      >
-        {children}
-      </form>
+    <div className="min-h-screen w-full flex justify-center items-center text-white bg-gradient-to-bl from-primary via-35% via-tertiary to-white flex-col gap-5 py-10">
+      <Link to="/">
+        <img src="./assets/logo-sinicrita.svg" alt="sinicrita" />
+      </Link>
+      <div className="w-full flex justify-center">
+        <form
+          onSubmit={onsubmit}
+          className="flex bg-primary p-7 md:p-10 rounded-2xl gap-7 w-10/12 md:w-1/2 lg:w-1/3 flex-col"
+          method="post"
+        >
+          {children}
+        </form>
+      </div>
     </div>
   );
 };
