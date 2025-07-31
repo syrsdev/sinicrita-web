415a952628601e5a6fabba6ad7564d7da09156f6
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-31 15:38:27 +0700

    refactor: move loading to file loading

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-    return (
-        <div className="flex-col gap-4 flex items-center justify-center w-min">
-            <div className="w-6 h-6 border-4 border-transparent text-white text-4xl animate-spin flex items-center justify-center border-t-white rounded-full"></div>
-        </div>
-    );
-};
-
-export default Loading;
