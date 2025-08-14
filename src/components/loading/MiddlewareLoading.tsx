5e0eb6f187239eb9abed3534690bf9c32481bfa9
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-08-14 23:36:31 +0700

    fix: sinicrita logo in loading

--- src/components/loading/MiddlewareLoading.tsx
+++ src/components/loading/MiddlewareLoading.tsx
@@ -3,7 +3,7 @@ import Loading from "./Loading";
 const MiddlewareLoading = () => {
   return (
     <div className="flex items-center justify-center flex-col h-screen gap-6">
-      <img src="./assets/logo-sinicrita.svg" alt="sinicrita" />
+      <img src="/assets/logo-sinicrita.svg" alt="sinicrita" />
       <span className="flex gap-3 text-xl">
         Loading.. <Loading color="text-primary border-t-primary" />
       </span>
