124fefc949b856e96a73ab5ef04926ad6f23d8a7
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-09-22 15:12:58 +0700

    style: responsive post page

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import PostCard from "../../components/post/Card";
 import MainSidebar from "../../components/sidebar/MainSidebar";
 import MainLayout from "../../layout/MainLayout";
-import { getPost } from "../../services/post.service";
+import { getGuestPost, getPost } from "../../services/post.service";
 import useAuth from "../../hooks/useAuth";
 
 const Post = () => {
@@ -10,14 +10,21 @@ const Post = () => {
   const { user } = useAuth();
 
   useEffect(() => {
-    getPost(user?.username, (res) =>
-      res.status == 404 ? setPost([]) : setPost(res.data.data)
-    );
+    let url: string = window.location.pathname;
+    if (url == "/") {
+      getGuestPost((res) =>
+        res.status == 404 ? setPost([]) : setPost(res.data.data)
+      );
+    } else {
+      getPost(user?.username, (res) =>
+        res.status == 404 ? setPost([]) : setPost(res.data.data)
+      );
+    }
   }, []);
 
   return (
     <MainLayout sidebar={<MainSidebar />} userLogin={user}>
-      <div className="w-4/6 h-screen overflow-y-auto pb-[70px]">
+      <div className="w-full md:w-4/6 h-screen overflow-y-auto pb-[70px]">
         {post.length == 0 || post == null ? (
           <div className="flex flex-col items-center gap-5 justify-center mt-5">
             <img
@@ -40,8 +47,8 @@ const Post = () => {
           ))
         )}
       </div>
-      <div className="w-2/6 border-s-2 border-border bg-main p-6">
-        <div className="bg-white w-full p-5 rounded-xl shadow-2xl flex flex-col gap-3 text-center justify-center sticky top-20">
+      <div className="hidden md:flex md:w-2/6 border-s-2 border-border bg-main p-6">
+        <div className="bg-white w-full p-5 h-fit rounded-xl shadow-2xl flex flex-col gap-3 text-center justify-center sticky top-20">
           <h3 className="text-[20px] font-semibold text-primary">
             💡Tips of the Day
           </h3>
