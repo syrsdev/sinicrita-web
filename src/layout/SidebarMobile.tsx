6c5a7524bae9ca25147d774c13704429bd04cda6
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-09-26 23:10:57 +0700

    style: sidebar mobile layout

--- src/layout/SidebarMobile.tsx
+++ src/layout/SidebarMobile.tsx
@@ -1,8 +1,19 @@
-
-const SidebarMobile = () => {
+const SidebarMobile = ({ isActive, onclick }: any) => {
   return (
-    <div>SidebarMobile</div>
-  )
-}
+    <>
+      {isActive && (
+        <div className="w-full flex lg:hidden min-h-screen top-0 absolute left-0 z-100">
+          <div
+            onClick={onclick}
+            className="w-1/5 md:w-1/2 bg-black min-h-screen  top-0 opacity-30"
+          ></div>
+          <div className="w-4/5 md:w-1/2 bg-white min-h-screen right-0 top-0 text-black px-5">
+            <h3 className="my-5">Sinicrita</h3>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default SidebarMobile
+export default SidebarMobile;
