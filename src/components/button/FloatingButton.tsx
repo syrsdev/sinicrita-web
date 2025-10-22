6ad333619d5b13f5ea2434d430fcd1df4aa3232e
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-10-22 18:04:23 +0700

    fix: position bottom sidebar conditionally

--- src/components/button/FloatingButton.tsx
+++ src/components/button/FloatingButton.tsx
@@ -7,7 +7,7 @@ const FloatingButton = ({ hiddenAddButton = false }: any) => {
       to={"/post/add"}
       className={`${
         hiddenAddButton ? "hidden" : ""
-      } flex items-center justify-center bg-primary font-bold text-[28px] text-white fixed bottom-5 right-5 w-12 h-12 rounded-lg`}
+      } flex items-center justify-center bg-primary font-bold text-[28px] text-white fixed bottom-20 lg:bottom-5 right-5 w-12 h-12 rounded-lg`}
     >
       <MdAdd />
     </NavLink>
