15bb4982a03fe71c2d131aa66831a17ef2dc0d12
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-10-23 20:15:04 +0700

    chore: remove unused class attribute

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -4,7 +4,7 @@ import { Tooltip } from "react-tooltip";
 const AuthLayout = ({ children, onsubmit }: any) => {
   return (
     <div className="min-h-screen w-full flex justify-center items-center text-white bg-gradient-to-bl from-primary via-35% via-tertiary to-white flex-col gap-5 py-10">
-      <Link to="/" className="">
+      <Link to="/">
         <img src="./assets/logo-sinicrita.svg" alt="sinicrita" />
       </Link>
       <div className="w-full flex justify-center">
